4b7922070f58d1dd936e4647f6ead871bb5a7163
Ramya D <ramyadhanush99@gmail.com>
2021-04-11 16:26:40 +0530

    Init : Created react-app and added files for homepage with scss
    Signed-off-by: Ramya D <ramyadhanush99@gmail.com>

---
+++ cart-club/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Homepage from './components/Homepage/Homepage.component';
+
+function App() {
+  return (
+     <Homepage/>
+  );
+}
+
+export default App;
---
+++ cart-club/src/components/Homepage/Homepage.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Homepage.styles.scss'
+const Homepage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+    
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="sub-title">SHOP NOW</span>
+        </div>
+      </div>
+      
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="sub-title">SHOP NOW</span>
+        </div>
+      </div>
+      
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="sub-title">SHOP NOW</span>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+);
+
+export default Homepage;
